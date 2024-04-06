3985cf7664b956d92d924cc8d32089bdb6f588be
Shaikh Sameer <samsk7774@gmail.com>
2024-04-07 03:37:33 +0530

    frontEnd-min

---
+++ frontend/react-ecommerce/src/features/product-list/ProductListAPI.js
@@ -0,0 +1,8 @@
+// A mock function to mimic making an async request for data
+export function fetchCount(amount = 1) {
+  return new Promise(async (resolve) => {
+    const responce = await fetch("http://localhost:8080");
+    const data = await responce.json();
+    resolve(data);
+  });
+}
